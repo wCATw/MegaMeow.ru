4876a7bf7908ab6497417405189accc8bf588f3d
Lev Kotovskiy <kotovskiy@studio-aw.ru>
2024-12-30 23:18:08 +0300

    add: publish workflow

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPO_DIR="/home/megameow/MegaMeow.ru"
+APP_DIR="/var/www/megameow"
+SERVICE_NAME="megameow.service"
+
+systemctl stop $SERVICE_NAME
+cd "$REPO_DIR"
+git reset --hard
+git fetch
+git checkout master
+git pull
+dotnet publish -c Release -o "$APP_DIR"
+systemctl start $SERVICE_NAME
